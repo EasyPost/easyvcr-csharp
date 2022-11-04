7b61164ac8158f1edba68fd2b273c1935a3c6faf
nharris <nharris@easypost.com>
2022-11-04 12:13:26 -0600

    - Add code coverage report generation dependencies to test project - Add code coverage report generation script and Makefile step - Add coveralls report upload step to GitHub CI

---
+++ generate_test_reports.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# install reportgenerator if not already installed
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 5.1.10 || true # exit 0 will kill the script, not what we want
+
+# check requirements
+set -- dotnet reportgenerator
+for req in "$@"; do
+    if ! command -v "$req" >/dev/null 2>&1; then
+        echo "$req could not be found"
+        exit
+    fi
+done
+
+# Generate Cobertura report for each test project
+test_folder="EasyVCR.Tests"
+cd "$test_folder" || exit
+dotnet test --collect:"XPlat Code Coverage"
+
+# Generate HTML and lcov report
+reportgenerator -reports:TestResults/*/coverage.cobertura.xml -targetdir:../coveragereport -reporttypes:Html
+reportgenerator -reports:TestResults/*/coverage.cobertura.xml -targetdir:../coveragereport -reporttypes:lcov
