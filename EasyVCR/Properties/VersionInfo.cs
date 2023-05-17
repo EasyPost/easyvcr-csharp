991546c70446226d8628b91900b719d357b7f32e
Nate Harris <nwithan8@users.noreply.github.com>
2023-05-17 14:49:31 -0600

    - Bump version number (#51)

--- EasyVCR/Properties/VersionInfo.cs
+++ EasyVCR/Properties/VersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 // Version information for an assembly must follow semantic versioning
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
