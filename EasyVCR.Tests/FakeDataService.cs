c4b8e4cd4d04e058c7445abab371280d1885a327
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-19 01:31:57 -0600

    - Linting

--- EasyVCR.Tests/FakeDataService.cs
+++ EasyVCR.Tests/FakeDataService.cs
@@ -43,40 +43,40 @@ namespace EasyVCR.Tests
         {
             _client = client;
         }
-        
+
         public static string JsonDataUrl => "https://api.ipify.org/";
 
         public static string XmlDataUrl => "http://restapi.adequateshop.com/api/Traveler";
-        
+
         public static string RawDataUrl => "https://api.ipify.org/?format=xml";
-        
+
         public async Task<HttpResponseMessage> GetJsonDataRawResponse()
         {
             return await Client.GetAsync(JsonDataUrl);
         }
-        
+
         public async Task<string?> GetJsonData()
         {
             var response = await GetJsonDataRawResponse();
             return await response.Content.ReadAsStringAsync();
         }
-        
+
         public async Task<HttpResponseMessage> GetXmlDataRawResponse()
         {
             return await Client.GetAsync(XmlDataUrl);
         }
-        
+
         public async Task<string?> GetXmlData()
         {
             var response = await GetXmlDataRawResponse();
             return await response.Content.ReadAsStringAsync();
         }
-        
+
         public async Task<HttpResponseMessage> GetRawDataRawResponse()
         {
             return await Client.GetAsync(RawDataUrl);
         }
-        
+
         public async Task<string?> GetRawData()
         {
             var response = await GetRawDataRawResponse();
