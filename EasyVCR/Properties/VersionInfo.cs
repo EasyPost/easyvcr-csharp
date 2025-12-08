745b45acaaa5bc6b1fb567ea6fb1cd4fc77fcefc
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-12-08 13:39:15 -0700

    chore: prep v0.13.0

--- EasyVCR/Properties/VersionInfo.cs
+++ EasyVCR/Properties/VersionInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 // Version information for an assembly must follow semantic versioning
-[assembly: AssemblyVersion("0.12.0")]
-[assembly: AssemblyFileVersion("0.12.0")]
-[assembly: AssemblyInformationalVersion("0.12.0")]
+[assembly: AssemblyVersion("0.13.0")]
+[assembly: AssemblyFileVersion("0.13.0")]
+[assembly: AssemblyInformationalVersion("0.13.0")]
